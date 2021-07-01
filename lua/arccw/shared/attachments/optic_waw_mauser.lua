ff123d0d9ee2a77f48c6e5ec410574c850c8971b
PalindroneV2 <asfadasfag@gmail.com>
2021-06-30 22:17:06 -0700

    Mauser M712 added.

---
+++ lua/arccw/shared/attachments/optic_waw_mauser.lua
@@ -0,0 +1,41 @@
+att.PrintName = "Mauser Scope (WAW)"
+att.Icon = Material("entities/acwatt_optic_waw_telescopic.png", "mips smooth")
+att.Description = "There is a black crosshair the inner portions being colored red. Ideally it should improve your aim."
+
+att.SortOrder = 100
+att.Free = true
+
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+
+att.Slot = "bo3_mauserscope"
+att.GivesFlags = {"boomhilda"}
+
+att.Model = "models/weapons/arccw/atts/bo3_mauserscope.mdl"
+att.ModelOffset = Vector(0, 0, 0)
+att.OffsetAng = Angle(0, 0, 0)
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 10, -1.165),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.25,
+        IgnoreExtra = true
+    },
+}
+
+att.Holosight = true
+att.HolosightReticle = Material("hud/scopes/bo3_mauserscope.png", "mips smooth")
+att.HolosightNoFlare = true
+att.HolosightSize = 12
+att.HolosightBone = "holosight"
+att.HolosightPiece = "models/weapons/arccw/atts/bo3_mauserscope_hsp.mdl"
+att.Colorable = true
+
+att.HolosightBlackbox = true
+
+att.HolosightMagnification = 2
+
+att.Mult_SightTime = 1.1
---
+++ lua/weapons/arccw_bo3_c96.lua
@@ -0,0 +1,339 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - World at War" -- edit this if you like
+SWEP.AdminOnly = false
+
+
+SWEP.PrintName = "Mauser M712"
+SWEP.Trivia_Class = "Pistol"
+SWEP.Trivia_Desc = "One of the first successful semi-automatic pistol designs. Saw use into World War 1 and even beyond World War 2 in limited capacity. This is the version that comes with a detachable box magazine and has the capacity to fire in fully automatic."
+SWEP.Trivia_Manufacturer = "Mauser"
+SWEP.Trivia_Calibre = "7.63x25mm Mauser"
+SWEP.Trivia_Mechanism = "Short Recoil. DA/SA"
+SWEP.Trivia_Country = "Belgium"
+SWEP.Trivia_Year = 1896
+
+SWEP.Slot = 1
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/c_bo3_c96.mdl"
+SWEP.WorldModel = "models/weapons/arccw/c_bo3_c96.mdl"
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    scale = 1.025,
+    pos        =    Vector(-9.5, 3, -3.5),
+    ang        =    Angle(-6, -2.5, 180),
+    bone    =    "ValveBiped.Bip01_R_Hand",
+}
+SWEP.ViewModelFOV = 60
+
+SWEP.Damage = 30
+SWEP.DamageMin = 25 -- damage done at maximum range
+SWEP.Range = 75 -- in METRES
+SWEP.Penetration = 6
+SWEP.DamageType = DMG_BULLET
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 425 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.CanFireUnderwater = true
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerCol = Color(255, 25, 25)
+SWEP.TracerWidth = 3
+
+SWEP.ChamberSize = 0 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 20 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 50
+
+SWEP.Recoil = 0.4
+SWEP.RecoilSide = 0.3
+SWEP.RecoilRise = 1
+
+SWEP.Delay = 60 / 950 -- 60 / RPM.
+SWEP.Num = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 2,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = "weapon_pistol"
+SWEP.NPCWeight = 100
+
+SWEP.AccuracyMOA = 3 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 250 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 150
+
+SWEP.ShootWhileSprint = false
+
+SWEP.Primary.Ammo = "pistol" -- what ammo type the gun uses
+SWEP.MagID = "c96" -- the magazine pool this gun draws from
+
+SWEP.ShootVol = 115 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+
+SWEP.ShootSound = "ArcCW_BO2.BHP_Fire"
+SWEP.ShootSoundSilenced = "ArcCW_BO2.Pistol_Sil"
+SWEP.DistantShootSound = "ArcCW_BO2.Pistol_RingOff"
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
+SWEP.ShellScale = 1.5
+SWEP.ShellPitch = 90
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+SWEP.ProceduralViewBobAttachment = 1
+SWEP.CamAttachment = 3
+
+SWEP.SightTime = 0.175
+SWEP.SpeedMult = 1
+SWEP.SightedSpeedMult = 0.75
+
+SWEP.BarrelLength = 18
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+    -- [0] = "bulletchamber",
+    -- [1] = "bullet1"
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-4.45, 0, -0.5),
+    Ang = Angle(0.25, 0.02, 0),
+    Magnification = 1.1,
+    CrosshairInSights = false,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+SWEP.HoldtypeHolstered = "normal"
+SWEP.HoldtypeActive = "pistol"
+SWEP.HoldtypeSights = "revolver"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
+
+SWEP.ActivePos = Vector(0, -3, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.SprintPos = Vector(0, -3, 0)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.CustomizePos = Vector(13, -3, -2)
+SWEP.CustomizeAng = Angle(15, 40, 20)
+
+SWEP.HolsterPos = Vector(0, -4, -5)
+SWEP.HolsterAng = Angle(37.5, 0, 0)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.AttachmentElements = {
+    ["ammo_papunch"] = {
+        --VMMaterial = "models/weapons/pap/pap_blue_burn",
+        NameChange = "Boomhilda",
+    },
+    ["boomhilda"] = {
+        VMBodygroups = {
+            {ind = 1, bg = 1},
+        },
+    },
+}
+
+SWEP.ExtraSightDist = 5
+
+SWEP.Attachments = {
+    { --1
+        PrintName = "Sights",
+        DefaultAttName = "Iron Sights",
+        Slot = {"bo3_mauserscope"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(2, 0.05, 2.375),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(7.238, 1.9, -2.622),
+            wang = Angle(90, 0, 0)
+        },
+    },
+    { --3
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Slot = "muzzle",
+        VMScale = Vector(1, 1, 1),
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(9, 0, 1.95),
+            vang = Angle(0, 0, 0),
+        },
+    },
+    { --4
+        PrintName = "Underbarrel",
+        Slot = {"foregrip_pistol", "style_pistol"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(2.5, 0.1, -0.3),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(7.238, 1.9, -2.622),
+            wang = Angle(90, 0, 0)
+        },
+    },
+    { --5
+        PrintName = "Tactical",
+        Slot = {"tac", "bo1_tacslot"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(4, 0, 0),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(8.5, 2, -2.9),
+            wang = Angle(-5, -2, 177.5)
+        },
+    },
+    { --6
+        PrintName = "Ammo Type",
+        Slot = {"ammo_pap"}
+    },
+    { --7
+        PrintName = "Perk",
+        Slot = "bo1_perk"
+    },
+    { --8
+        PrintName = "Charms",
+        Slot = "charm",
+        Bone = "tag_weapon",
+        VMScale = Vector(1, 1, 1),
+        Offset = {
+            vpos = Vector(6, -0.1, 1.1),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(8.5, 2.5, -4),
+            wang = Angle(-5, -2, 177.5)
+        },
+    },
+}
+
+SWEP.Hook_ModifyBodygroups = function(wep, data)
+    local vm = data.vm
+    local papcamo = wep:GetBuff_Override("PackAPunch")
+
+    if papcamo then
+        wep.Penetration = 20
+        return vm:SetSkin(1)
+    else
+        wep.Penetration = 6
+    end
+end
+
+SWEP.Hook_GetShootSound = function(wep, sound)
+    if wep.Attachments[3].Installed and wep:GetBuff_Override("Silencer") then
+        return "ArcCW_BO2.Pistol_Sil"
+    end
+end
+
+SWEP.counter = 0
+
+SWEP.Hook_TranslateAnimation = function(wep, anim, data)
+    local mauserscope = wep.Attachments[1].Installed == "optic_waw_mauser"
+
+    if mauserscope then
+        return anim .. "_scope"
+    end
+end
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1 / 30,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 0.5,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["holster"] = {
+        Source = "holster",
+        Time = 0.5,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["ready"] = {
+        Source = "first_draw",
+        Time = 1,
+        SoundTable = {
+            {s = "ArcCW_BO2.Pistol_SlideBack", t = 0.2},
+            {s = "ArcCW_BO2.Pistol_SlideFwd", t = 0.8}
+        }
+    },
+    ["ready_scope"] = {
+        Source = "first_draw_scope",
+        Time = 1,
+        SoundTable = {
+            {s = "ArcCW_BO2.Pistol_SlideBack", t = 0.2},
+            {s = "ArcCW_BO2.Pistol_SlideFwd", t = 0.8}
+        }
+    },
+    ["fire"] = {
+        Source = {"fire"},
+        Time = 8 / 30,
+        ShellEjectAt = 1 / 30,
+    },
+    ["fire_iron"] = {
+        Source = "fire_ads",
+        Time = 8 / 30,
+        ShellEjectAt = 1 / 30,
+    },
+    ["fire_iron_scope"] = {
+        Source = "fire_ads_scope",
+        Time = 8 / 30,
+        ShellEjectAt = 1 / 30,
+    },
+    ["reload"] = {
+        Source = "reload",
+        Time = 1.5,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_PISTOL,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.2,
+        SoundTable = {
+            {s = "ArcCW_BO2.Pistol_MagOut", t = 0.25},
+            {s = "ArcCW_BO2.Pistol_MagIn", t = 1}
+        },
+    },
+    ["reload_empty"] = {
+        Source = "reload_empty",
+        Time = 2,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_PISTOL,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.2,
+        SoundTable = {
+            {s = "ArcCW_BO2.Pistol_MagOut", t = 0.25},
+            {s = "ArcCW_BO2.Pistol_MagIn", t = 1},
+            {s = "ArcCW_BO2.Pistol_SlideFwd", t = 1.5}
+        },
+    },
+    ["enter_sprint"] = {
+        Source = "sprint_in",
+        Time = 1 / 30
+    },
+    ["idle_sprint"] = {
+        Source = "sprint_loop",
+        Time = 30 / 40
+    },
+    ["exit_sprint"] = {
+        Source = "sprint_out",
+        Time = 1 / 30
+    },
+}
