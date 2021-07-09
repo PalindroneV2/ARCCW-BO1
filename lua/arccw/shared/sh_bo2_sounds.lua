85b745ccd9ea77e414395e3f3c2a102072774c1d
PalindroneV2 <asfadasfag@gmail.com>
2021-07-08 19:28:17 -0700

    S12 in. Added M1216 sounds. Added shotgun dists.
    New Model Army moved to WAW. $nocull 1 on g36 textures.

--- lua/arccw/shared/sh_bo2_sounds.lua
+++ lua/arccw/shared/sh_bo2_sounds.lua
@@ -308,6 +308,16 @@ sound.Add({
     }
 })
 sound.Add({
+    name = "ArcCW_BO2.1216_Fire",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    --pitch = {95, 110},
+    sound = {
+        "weapons/arccw/bo2_generic_shotgun/fire_1216.wav",
+    }
+})
+sound.Add({
     name = "ArcCW_BO2.S12_Sil",
     channel = CHAN_STATIC,
     volume = 1.0,
---
+++ lua/weapons/arccw_bo2_s12.lua
@@ -0,0 +1,430 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - Black Ops II" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "Saiga 12K"
+SWEP.Trivia_Class = "Combat Shotgun"
+SWEP.Trivia_Desc = "12 gauge semi automatic combat shotgun based on the AK platform. Unlike many self loading shotguns which run into feeding issues, the Saiga manages to be reliable due to the loser tolerances of the AK platform it was based on."
+SWEP.Trivia_Manufacturer = "Lone Star Arms"
+SWEP.Trivia_Calibre = "12x70mm"
+SWEP.Trivia_Mechanism = "Semi-Automatic"
+SWEP.Trivia_Country = "Russia"
+SWEP.Trivia_Year = 1990
+
+SWEP.Slot = 3
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/c_bo2_s12.mdl"
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    pos        =    Vector(-1, 4, -6),
+    ang        =    Angle(-10, -1, 180),
+    bone    =    "ValveBiped.Bip01_R_Hand",
+    scale   =   1
+}
+SWEP.WorldModel = "models/weapons/arccw/w_bo2_s12.mdl"
+SWEP.MirrorWorldModel = "models/weapons/arccw/w_bo2_s12.mdl"
+SWEP.ViewModelFOV = 60
+
+SWEP.DefaultBodygroups = "000000000"
+
+SWEP.Damage = 40
+SWEP.DamageMin = 20 -- damage done at maximum range
+SWEP.Range = 55 -- in METRES
+SWEP.Penetration = 1
+SWEP.DamageType = DMG_BUCKSHOT
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 1000 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerCol = Color(255, 25, 25)
+SWEP.TracerWidth = 3
+
+SWEP.ChamberSize = 0 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 10 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 20
+SWEP.ReducedClipSize = 5
+
+SWEP.Recoil = 2
+SWEP.RecoilSide = 2
+SWEP.MaxRecoilBlowback = 2
+
+SWEP.Delay = 60 / 468 -- 60 / RPM.
+SWEP.Num = 8 -- number of shots per trigger pull.
+SWEP.RunawayBurst = false
+SWEP.Firemodes = {
+    {
+        Printname = "SEMI-AUTO",
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = "weapon_shotgun"
+SWEP.NPCWeight = 100
+
+SWEP.AccuracyMOA = 40 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 650 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 150
+
+SWEP.Primary.Ammo = "buckshot" -- what ammo type the gun uses
+
+SWEP.ShootVol = 120 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+
+SWEP.ShootSound = "ArcCW_BO2.1216_Fire"
+SWEP.ShootSoundSilenced = "ArcCW_BO2.S12_Sil"
+SWEP.DistantShootSound = {"weapons/arccw/bo2_generic_shotgun/dist/0.wav", "weapons/arccw/bo2_generic_shotgun/dist/1.wav"}
+
+SWEP.MuzzleEffect = "muzzleflash_shotgun"
+SWEP.ShellModel = "models/shells/shell_12gauge.mdl"
+SWEP.ShellPitch = 100
+SWEP.ShellSounds = ArcCW.ShotgunShellSoundsTable
+SWEP.ShellScale = 1.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+SWEP.ProceduralViewBobAttachment = 1
+SWEP.CamAttachment = 3
+
+SWEP.SpeedMult = 0.95
+SWEP.SightedSpeedMult = 0.75
+SWEP.SightTime = 0.27
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+    -- [0] = "bulletchamber",
+    -- [1] = "bullet1"
+}
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-2.7, 0, 0.5),
+    Ang = Angle(00, 0, 0),
+    Magnification = 1.1,
+    CrosshairInSights = false,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "shotgun"
+SWEP.HoldtypeSights = "ar2"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN
+
+SWEP.ActivePos = Vector(1, 3, 0.25)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.SprintPos = Vector(0, 3, 0)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.CustomizePos = Vector(17, 6.5, -2)
+SWEP.CustomizeAng = Angle(15, 40, 20)
+
+SWEP.HolsterPos = Vector(0.532, -6, 0)
+SWEP.HolsterAng = Angle(-7.036, 30.016, 0)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.AttachmentElements = {
+    ["spas12_pap"] = {
+        --VMMaterial = "models/weapons/pap/pap_blue_burn",
+        NamePriority = 10,
+        NameChange = "Synthetic Dozen",
+    },
+    ["solider_stock"] = {
+        VMBodygroups = {
+            {ind = 3, bg = 3},
+        },
+    },
+    ["solid_stock"] = {
+        VMBodygroups = {
+            {ind = 3, bg = 2},
+        },
+    },
+    ["light_stock"] = {
+        VMBodygroups = {
+            {ind = 3, bg = 1},
+        }
+    },
+    ["mount"] = {
+        VMBodygroups = {
+            {ind = 2, bg = 1},
+        },
+    },
+    ["bo2_fastmags"] = {
+        VMBodygroups = {
+            {ind = 1, bg = 1},
+        },
+    },
+}
+
+SWEP.ExtraSightDist = 5
+
+SWEP.Attachments = {
+    {
+        PrintName = "Optic", -- print name
+        DefaultAttName = "Iron Sights",
+        Slot = {"optic"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(0, 0, 2.3), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+            wpos = Vector(12.5, 1.3, -7),
+            wang = Angle(172.5, 180, 0)
+        },
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(0, 0, 0),
+        InstalledEles = {"mount"},
+    }, --1
+    {
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Slot = "muzzle_shotgun",
+        Bone = "tag_weapon",
+        VMScale = Vector(1.5, 1.5, 1.5),
+        Offset = {
+            vpos = Vector(24, 0, 0.36), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+        },
+    }, --2
+    {
+        PrintName = "Underbarrel",
+        Slot = "foregrip",
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(8.5, 0, -1.25),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(21.5, 0.75, -3.25),
+            wang = Angle(172.5, 0, 0)
+        },
+        SlideAmount = false
+    }, --3
+    {
+        PrintName = "Tactical",
+        Slot = {"bo1_tacprimary"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(12.5, -1, 0.25), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 90),
+        },
+    }, --4
+    {
+        PrintName = "Stock",
+        Slot = {"bo1_stock", "bo1_mp5stock"},
+        DefaultAttName = "No Stock",
+    }, --5
+    {
+        PrintName = "Magazine",
+        DefaultAttName = "Standard Mag.",
+        Slot = "bo1_mag",
+    }, --6
+    { --6
+        PrintName = "FCG",
+        Slot = {"bo1_fcg"}
+    },
+    {
+        PrintName = "Ammo Type",
+        Slot = {"ammo_spas12_pap"},
+    }, --7
+    {
+        PrintName = "Perk",
+        Slot = "bo1_perk",
+    }, --8
+    {
+        PrintName = "Charm",
+        Slot = "charm",
+        FreeSlot = true,
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(4, -0.65, 0), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+            wpos = Vector(7, 1.6, -4),
+            wang = Angle(-10, 0, 180)
+        },
+    }, --8
+}
+
+SWEP.Hook_ModifyBodygroups = function(wep, data)
+    local vm = data.vm
+    local papcamo = wep.Attachments[8].Installed == "ammo_spas12_pap"
+
+    if papcamo then
+        return vm:SetSkin(2)
+    end
+end
+
+SWEP.Hook_SelectReloadAnimation = function(wep, curanim)
+    local fastmag = wep:GetBuff_Override("BO1_FastMag")
+
+    if curanim == "reload" and fastmag then
+        return curanim .. "_fast"
+    end
+    if curanim == "reload_empty" and fastmag then
+        return curanim .. "_fast"
+    end
+end
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1 / 35,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["holster"] = {
+        Source = "holster",
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["idle_empty"] = {
+        Source = "idle_empty",
+        Time = 1 / 35,
+    },
+    ["draw_empty"] = {
+        Source = "draw_empty",
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["holster_empty"] = {
+        Source = "holster_empty",
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["ready"] = {
+        Source = "first_draw",
+        Time = 59 / 30,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.2,
+        SoundTable = {
+            {s = "ArcCW_BO2.AR_Back", t = 0.5 / 1.15},
+            {s = "ArcCW_BO2.AR_Fwd", t = 0.7 / 1.15},
+        },
+    },
+    ["fire"] = {
+        Source = {
+            "fire",
+        },
+        Time = 9 / 30,
+        ShellEjectAt = 0.15,
+    },
+    ["fire_iron"] = {
+        Source = {
+            "fire_ads",
+        },
+        Time = 9 / 30,
+        ShellEjectAt = 0.15,
+    },
+    ["fire_empty"] = {
+        Source = {
+            "fire_last",
+        },
+        Time = 9 / 30,
+        ShellEjectAt = 0.15,
+    },
+    ["fire_iron_empty"] = {
+        Source = {
+            "fire_ads_last",
+        },
+        Time = 9 / 30,
+        ShellEjectAt = 0.15,
+    },
+    ["reload"] = {
+        Source = "reload",
+        Time = 3.5 / 1.15,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.4,
+        SoundTable = {
+            {s = "ArcCW_BO2.AR_MagOut", t = 0.5 / 1.15},
+            {s = "ArcCW_BO2.AR_MagIn", t = 2.4 / 1.15},
+        },
+    },
+    ["reload_empty"] = {
+        Source = "reload_empty",
+        Time = 4.16 / 1.15,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.4,
+        SoundTable = {
+            {s = "ArcCW_BO2.AR_Back", t = 0.5 / 1.15},
+            {s = "ArcCW_BO2.AR_MagOut", t = 1.1 / 1.15},
+            {s = "ArcCW_BO2.AR_MagIn", t = 2.4 / 1.15},
+            {s = "ArcCW_BO2.AR_Back", t = 2.8 / 1.15},
+            {s = "ArcCW_BO2.AR_Fwd", t = 3 / 1.15},
+        },
+    },
+    ["reload_fast"] = {
+        Source = "reload_fast",
+        Time = (1.8 * 1.25) / 1.15,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.4,
+        SoundTable = {
+            {s = "ArcCW_BO2.AR_MagOut", t = 0.5 / 1.15},
+            {s = "ArcCW_BO2.AR_MagIn", t = 1 / 1.15},
+        },
+        MinProgress = 1,
+    },
+    ["reload_empty_fast"] = {
+        Source = "reload_empty_fast",
+        Time = (2.633 * 1.25) / 1.15,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.4,
+        MinProgress = 1.8,
+        SoundTable = {
+            {s = "ArcCW_BO2.AR_MagOut", t = 0.5 / 1.15},
+            {s = "ArcCW_BO2.AR_MagIn", t = 1 / 1.15},
+            {s = "ArcCW_BO2.AR_Back", t = 1.8 / 1.15},
+            {s = "ArcCW_BO2.AR_Fwd", t = 2 / 1.15},
+        },
+    },
+    ["enter_sprint"] = {
+        Source = "sprint_in",
+        Time = 10 / 30
+    },
+    ["idle_sprint"] = {
+        Source = "sprint_loop",
+        Time = 30 / 40
+    },
+    ["exit_sprint"] = {
+        Source = "sprint_out",
+        Time = 10 / 30
+    },
+    ["enter_sprint_empty"] = {
+        Source = "sprint_in_empty",
+        Time = 10 / 30
+    },
+    ["idle_sprint_empty"] = {
+        Source = "sprint_loop_empty",
+        Time = 30 / 40
+    },
+    ["exit_sprint_empty"] = {
+        Source = "sprint_out_empty",
+        Time = 10 / 30
+    },
+}
