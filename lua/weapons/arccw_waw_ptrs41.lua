c9c3771bab1b63ce550d350c14adf073dd4eb206
PalindroneV2 <asfadasfag@gmail.com>
2021-08-24 22:15:08 -0700

    PTRS-41 added.

--- lua/weapons/arccw_waw_ptrs41.lua
+++ lua/weapons/arccw_waw_ptrs41.lua
@@ -1,36 +1,38 @@
 SWEP.Base = "arccw_base"
 SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - Black Ops" -- edit this if you like
+SWEP.Category = "ArcCW - World at War" -- edit this if you like
 SWEP.AdminOnly = false
 
-SWEP.PrintName = "Barrett M82"
+SWEP.PrintName = "PTRS-41"
 SWEP.Trivia_Class = "Anti-Materiel Precision Rifle"
 SWEP.Trivia_Desc = [[
-    American .50 caliber precision rifle. Very heavy, very powerful and extremely dangerous in the right hands.
+    Russian anti-materiel rifle. Made for destroying armored vehicles and tanks. It's devastating anti-tank power comes at the cost of a massive 2 meter length and weight that makes it unable to be fired from the shoulder.
 ]]
-SWEP.Trivia_Manufacturer = "Barrett"
-SWEP.Trivia_Calibre = ".50 BMG"
-SWEP.Trivia_Mechanism = "Recoil Operated, Rotating Bolt"
-SWEP.Trivia_Country = "USA"
-SWEP.Trivia_Year = 1980
+SWEP.Trivia_Manufacturer = "Many"
+SWEP.Trivia_Calibre = "14.5x144mm"
+SWEP.Trivia_Mechanism = "Recoil Operated, Tilting Bolt"
+SWEP.Trivia_Country = "USSR"
+SWEP.Trivia_Year = 1941
 
 SWEP.Slot = 3
 
 SWEP.UseHands = true
 
-SWEP.ViewModel = "models/weapons/arccw/c_bo2_m82.mdl"
-SWEP.WorldModel = "models/weapons/arccw/c_bo2_m82.mdl"
+SWEP.ViewModel = "models/weapons/arccw/c_waw_ptrs41.mdl"
+SWEP.WorldModel = "models/weapons/arccw/w_waw_ptrs41.mdl"
+SWEP.MirrorWorldModel = "models/weapons/arccw/w_waw_ptrs41.mdl"
 SWEP.MirrorVMWM = true
 SWEP.WorldModelOffset = {
-    pos        =    Vector(-4.5, 4, -7.5),
-    ang        =    Angle(-6, 0, 180),
+    pos        =    Vector(-6, 6.5, -8),
+    ang        =    Angle(-10, 0, 180),
     bone    =    "ValveBiped.Bip01_R_Hand",
+    scale = 0.9
 }
 SWEP.ViewModelFOV = 60
 
 SWEP.DefaultBodygroups = "00010000000"
 
-SWEP.Damage = 150
+SWEP.Damage = 175
 SWEP.DamageMin = 120 -- damage done at maximum range
 SWEP.Range = 1400 -- in METRES
 SWEP.Penetration = 20
@@ -50,11 +52,11 @@ SWEP.ExtendedClipSize = 20
 SWEP.Recoil = 5
 SWEP.RecoilSide = 2
 SWEP.RecoilRise = 2
-SWEP.SpeedMult = 0.7
+SWEP.SpeedMult = 0.5
 SWEP.SightedSpeedMult = 0.35
-SWEP.SightTime = 0.4
+SWEP.SightTime = 0.65
 
-SWEP.Delay = 60 / 416 -- 60 / RPM.
+SWEP.Delay = 60 / 75 -- 60 / RPM.
 SWEP.Num = 1 -- number of shots per trigger pull.
 SWEP.Firemodes = {
     {
@@ -75,7 +77,7 @@ SWEP.HipDispersion = 500 -- inaccuracy added by hip firing.
 SWEP.MoveDispersion = 250
 
 SWEP.Primary.Ammo = "SniperPenetratedRound" -- what ammo type the gun uses
-SWEP.MagID = "svd" -- the magazine pool this gun draws from
+SWEP.MagID = "ptrs41" -- the magazine pool this gun draws from
 
 SWEP.ShootVol = 110 -- volume of shoot sound
 SWEP.ShootPitch = 100 -- pitch of shoot sound
@@ -92,7 +94,7 @@ SWEP.ShellScale = 2
 SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
 SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
 SWEP.ProceduralViewBobAttachment = 1
-SWEP.CamAttachment = 4
+SWEP.CamAttachment = 3
 
 SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
     -- [0] = "bulletchamber",
@@ -105,10 +107,10 @@ SWEP.ProceduralIronFire = false
 SWEP.CaseBones = {}
 
 SWEP.IronSightStruct = {
-    Pos = Vector(-2.91, 4, 1),
-    Ang = Angle(0.1, 0.05, 0),
+    Pos = Vector(-5, 2, 3.75),
+    Ang = Angle(0.25, 0.2, 0),
     Magnification = 1.1,
-    CrosshairInSights = true,
+    CrosshairInSights = false,
     SwitchToSound = "", -- sound that plays when switching to this sight
 }
 
@@ -121,8 +123,8 @@ SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
 SWEP.ActivePos = Vector(0, 2, 1)
 SWEP.ActiveAng = Angle(0, 0, 0)
 
-SWEP.SprintPos = Vector(0, 2, 1)
-SWEP.SprintAng = Angle(0, 0, 0)
+SWEP.SprintPos = Vector(3, 2, 0)
+SWEP.SprintAng = Angle(0, 30, 0)
 
 SWEP.CustomizePos = Vector(20, 5, -4.5)
 SWEP.CustomizeAng = Angle(15, 40, 20)
@@ -137,28 +139,21 @@ SWEP.BarrelLength = 35
 
 SWEP.AttachmentElements = {
     ["rail"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 1},
-        },
-    },
-    ["bo1_bipod"] = {
-        VMBodygroups = {
-            {ind = 3, bg = 2}
-        },
-    },
-    ["muzzle"] = {
-        VMBodygroups = {
-            {ind = 4, bg = 1}
-        },
-    },
-    ["ub"] = {
-        VMBodygroups = {
-            {ind = 3, bg = 0}
+        VMElements = {
+            {
+                Model = "models/weapons/arccw/item/bo1_ak_rail.mdl",
+                Bone = "tag_weapon",
+                Scale = Vector(0.375, 0.375, 0.375),
+                Offset = {
+                    pos = Vector(3.25, 0.325, 1),
+                    ang = Angle(0, 90, 0),
+                }
+            },
         },
     },
     ["ammo_papunch"] = {
         NamePriority = 10,
-        NameChange = "Macro Annihilator",
+        NameChange = "The Penetrator",
     },
 }
 
@@ -171,13 +166,14 @@ SWEP.Attachments = {
     {
         PrintName = "Optic", -- print name
         DefaultAttName = "Iron Sights",
-        Slot = {"optic_sniper", "optic", "bo2_m82scope"}, -- what kind of attachments can fit here, can be string or table
+        Slot = {"optic_sniper", "optic"}, -- what kind of attachments can fit here, can be string or table
         Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
         Offset = {
-            vpos = Vector(4, 0, 3.8),
+            vpos = Vector(3.25, 0.025, 2.15),
             vang = Angle(0, 0, 0),
         },
         InstalledEles = {"rail"},
+        MergeSlots = {7}
     },--1
     {
         PrintName = "Muzzle",
@@ -187,45 +183,24 @@ SWEP.Attachments = {
         WMScale = Vector(4, 2.5, 2.5),
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(37, 0, 2.3),
+            vpos = Vector(73, 0, 0),
             vang = Angle(0, 0, 90),
         },
-        InstalledEles = {"muzzle"}
     },--2
-    {
-        PrintName = "Underbarrel",
-        Slot = {"foregrip", "bo1_bipod"},
-        Bone = "tag_weapon",
-        Offset = {
-            vpos = Vector(16, 0, 1.25),
-            vang = Angle(0, 0, 0),
-        },
-        MergeSlots = {4},
-        InstalledEles = {"ub"}
-    },--3
-    { --4
-        Hidden = true,
-        Slot = {"bipod"},
-        Bone = "tag_weapon",
-        Offset = {
-            vpos = Vector(21, 0, 1.75),
-            vang = Angle(0, 0, 0),
-        },
-    },
-    { --5
+    { --3
         PrintName = "Tactical",
         Slot = {"bo1_tacprimary"},
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(19, 0, 3.75),
-            vang = Angle(0, 0, 180),
+            vpos = Vector(20, 1, 0),
+            vang = Angle(0, 0, -90),
         },
     },
-    { --6
+    { --4
         PrintName = "Ammo Type",
         Slot = {"ammo_pap"}
     },
-    { --7
+    { --5
         PrintName = "Perk",
         Slot = "bo1_perk"
     },
@@ -235,15 +210,24 @@ SWEP.Attachments = {
         FreeSlot = true,
         Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
         Offset = {
-            vpos = Vector(6.5, -0.5, 1.5),
+            vpos = Vector(4, -1, 0),
             vang = Angle(0, 0, 0),
         },
-    },--8
+    },--6
+    {
+        Hidden = true,
+        Slot = "waw_pem_scope",
+        Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(0, 0, 0),
+            vang = Angle(0, 0, 0),
+        },
+    },--7
 }
 
 SWEP.Hook_ModifyBodygroups = function(wep, data)
     local vm = data.vm
-    local papcamo = wep.Attachments[6].Installed == "ammo_papunch"
+    local papcamo = wep:GetBuff_Override("PackAPunch")
 
     if papcamo then return vm:SetSkin(3) end
 end
@@ -253,16 +237,34 @@ SWEP.Animations = {
         Source = "idle",
         Time = 1 / 35,
     },
+    ["idle_empty"] = {
+        Source = "idle_empty",
+        Time = 1 / 35,
+    },
     ["draw"] = {
         Source = "draw",
-        Time = 1.86,
+        Time = 1,
         LHIK = true,
         LHIKIn = 0.2,
         LHIKOut = 0.25,
     },
     ["holster"] = {
         Source = "holster",
-        Time = 1.45,
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["draw_empty"] = {
+        Source = "draw_empty",
+        Time = 1,
+        LHIK = true,
+        LHIKIn = 0.2,
+        LHIKOut = 0.25,
+    },
+    ["holster_empty"] = {
+        Source = "holster_empty",
+        Time = 1,
         LHIK = true,
         LHIKIn = 0.2,
         LHIKOut = 0.25,
@@ -275,13 +277,23 @@ SWEP.Animations = {
         LHIKOut = 0.25,
     },
     ["fire"] = {
-        Source = {"shoot"},
-        Time = 13 / 35,
+        Source = {"fire"},
+        Time = 7 / 35,
         ShellEjectAt = 0,
     },
     ["fire_iron"] = {
-        Source = "shoot",
-        Time = 13 / 35,
+        Source = "fire_ads",
+        Time = 7 / 35,
+        ShellEjectAt = 0,
+    },
+    ["fire_empty"] = {
+        Source = {"fire_last"},
+        Time = 7 / 35,
+        ShellEjectAt = 0,
+    },
+    ["fire_iron_empty"] = {
+        Source = "fire_ads_last",
+        Time = 7 / 35,
         ShellEjectAt = 0,
     },
     ["reload"] = {
@@ -313,17 +325,5 @@ SWEP.Animations = {
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.5,
-    },
-    ["enter_sprint"] = {
-        Source = "sprint_in",
-        Time = 10 / 30
-    },
-    ["idle_sprint"] = {
-        Source = "sprint_loop",
-        Time = 30 / 40
-    },
-    ["exit_sprint"] = {
-        Source = "sprint_out",
-        Time = 10 / 30
     },
 }
