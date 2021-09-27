8d1008c9a3d6e8da5710f2c9cd6c30bbd6893146
TheOnly8Z <888888zombies@gmail.com>
2021-09-27 17:27:34 -0400

    LC10 balance

--- lua/arccw/shared/attachments/bo1_fcg_rapid_lc10.lua
+++ lua/arccw/shared/attachments/bo1_fcg_rapid_lc10.lua
@@ -1,4 +1,5 @@
-att.PrintName = "Heavy Modification"
+att.PrintName = "MPL Heavy Modification"
+att.AbbrevName = "Heavy Modification"
 att.Icon = Material("entities/acwatt_bo1_fcg_rapid.png", "mips smooth")
 att.Description = "Modified furniture based on the Arctic Warfare and an improved sear and trigger mechanism make the MPL incredibly fast."
 att.Desc_Pros = {
@@ -8,6 +9,12 @@ att.Desc_Cons = {
 att.AutoStats = true
 att.Slot = "bo1_fcg_lc10"
 att.SortOrder = 100
+
 att.Mult_RPM = 1.35
+att.Mult_Recoil = 0.85
+
 att.Mult_SightTime = 1.25
-att.Mult_SightedSpeedMult = 1.15
+att.Mult_SightedSpeedMult = 0.85
+att.Mult_SpeedMult = 0.975
+
+att.Mult_ShootPitch = 0.9
