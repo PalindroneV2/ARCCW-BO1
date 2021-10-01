dc83445c0bd916a3cb9987324bf5073d9280929c
PalindroneV2 <asfadasfag@gmail.com>
2021-10-01 10:26:19 -0700

    ballista textures

--- lua/weapons/arccw_bo2_ballista.lua
+++ lua/weapons/arccw_bo2_ballista.lua
@@ -1,37 +1,36 @@
 SWEP.Base = "arccw_base"
 SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - Black Ops II" -- edit this if you like
+SWEP.Category = "ArcCW - Black Ops" -- edit this if you like
 SWEP.AdminOnly = false
 
-SWEP.PrintName = "DSR-50"
-SWEP.Trivia_Class = "Antimateriel Rifle"
-SWEP.Trivia_Desc = "Antimateriel sniper rifle designed in germany. Much more stable and precise than other .50 BMG rifles."
-SWEP.Trivia_Manufacturer = "DSR-Precision"
-SWEP.Trivia_Calibre = ".50 BMG"
+SWEP.PrintName = "FN Ballista"
+SWEP.Trivia_Class = "Sniper Rifle"
+SWEP.Trivia_Desc = "A high-caliber sniper rifle designed for cold-weather police and military units. Known for once holding the record of the longest range sniper shot in history."
+SWEP.Trivia_Manufacturer = "FN Herstal"
+SWEP.Trivia_Calibre = ".338 Lapua"
 SWEP.Trivia_Mechanism = "Bolt-Action"
-SWEP.Trivia_Country = "Germany"
-SWEP.Trivia_Year = 2003
+SWEP.Trivia_Country = "Belgium"
+SWEP.Trivia_Year = 2012
 
 SWEP.Slot = 3
 
-SWEP.ViewModel = "models/weapons/arccw/c_bo2_dsr50.mdl"
+SWEP.ViewModel = "models/weapons/arccw/c_bo1_awm.mdl"
 SWEP.MirrorVMWM = true
 SWEP.WorldModelOffset = {
-    pos        =    Vector(-9.25, 4.5, -6.5),
-    ang        =    Angle(-6, -1, 180),
+    pos        =    Vector(-6, 4.75, -7),
+    ang        =    Angle(-6, -2.5, 180),
     bone    =    "ValveBiped.Bip01_R_Hand",
 }
-SWEP.WorldModel = "models/weapons/arccw/w_bo2_dsr50.mdl"
-SWEP.MirrorWorldModel = "models/weapons/arccw/w_bo2_dsr50.mdl"
+SWEP.WorldModel = "models/weapons/arccw/c_bo1_awm.mdl"
 SWEP.ViewModelFOV = 60
 
-SWEP.Damage = 200
-SWEP.DamageMin = 115 -- damage done at maximum range
-SWEP.Range = 1000 -- in METRES
-SWEP.Penetration = 20
+SWEP.Damage = 120
+SWEP.DamageMin = 110 -- damage done at maximum range
+SWEP.Range = 2000 -- in METRES
+SWEP.Penetration = 12
 SWEP.DamageType = DMG_BULLET
 SWEP.ShootEntity = nil -- entity to fire, if any
-SWEP.MuzzleVelocity = 853 -- projectile or phys bullet muzzle velocity
+SWEP.MuzzleVelocity = 850 -- projectile or phys bullet muzzle velocity
 -- IN M/S
 
 SWEP.TracerNum = 1 -- tracer every X
@@ -42,6 +41,9 @@ SWEP.ChamberSize = 0 -- how many rounds can be chambered.
 SWEP.Primary.ClipSize = 5 -- DefaultClip is automatically set.
 SWEP.ExtendedClipSize = 10
 SWEP.ReducedClipSize = 5
+
+SWEP.Recoil = 4
+SWEP.RecoilSide = 2
 
 SWEP.Delay = 60 / 45 -- 60 / RPM.
 SWEP.Num = 1 -- number of shots per trigger pull.
@@ -63,17 +65,17 @@ SWEP.NPCWeight = 100
 
 SWEP.ManualAction = true
 
-SWEP.AccuracyMOA = 0.2 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.AccuracyMOA = 0.1 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
 SWEP.HipDispersion = 800 -- inaccuracy added by hip firing.
 SWEP.MoveDispersion = 100
 
 SWEP.Primary.Ammo = "SniperPenetratedRound" -- what ammo type the gun uses
-SWEP.MagID = "dsr50" -- the magazine pool this gun draws from
+SWEP.MagID = "hs338" -- the magazine pool this gun draws from
 
 SWEP.ShootVol = 140 -- volume of shoot sound
 SWEP.ShootPitch = 100 -- pitch of shoot sound
 
-SWEP.ShootSound = "ArcCW_BO2.DSR50_Fire"
+SWEP.ShootSound = "ArcCW_BO1.Ballista_Fire"
 SWEP.ShootSoundSilenced = "ArcCW_BO2.Ballista_Sil"
 SWEP.DistantShootSound = {"^weapons/arccw/bo2_generic_sniper/dist/flux_l.wav", "^weapons/arccw/bo2_generic_sniper/dist/flux_r.wav"}
 
@@ -87,12 +89,9 @@ SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
 SWEP.ProceduralViewBobAttachment = 1
 SWEP.CamAttachment = 4
 
-SWEP.Recoil = 5
-SWEP.RecoilSide = 2
-SWEP.RecoilRise = 2
-SWEP.SpeedMult = 0.7
-SWEP.SightedSpeedMult = 0.35
-SWEP.SightTime = 0.4
+SWEP.SightTime = 0.45
+SWEP.SpeedMult = 0.85
+SWEP.SightedSpeedMult = 0.25
 
 SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
     -- [0] = "bulletchamber",
@@ -105,10 +104,9 @@ SWEP.ProceduralIronFire = true
 SWEP.CaseBones = {}
 
 SWEP.IronSightStruct = {
-    Pos = Vector(-3.8, 3, 1.6),
-    Ang = Angle(-0.85, 0.01, 0),
+    Pos = Vector(-4.3, 0, 2.35),
+    Ang = Angle(0, 0, 0),
     Magnification = 1.1,
-    CrosshairInSights = false
 }
 
 SWEP.HoldtypeHolstered = "passive"
@@ -117,14 +115,11 @@ SWEP.HoldtypeSights = "rpg"
 
 SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_RPG
 
-SWEP.ActivePos = Vector(0, 1, 0.5)
+SWEP.ActivePos = Vector(-1, 3, 1)
 SWEP.ActiveAng = Angle(0, 0, 0)
 
-SWEP.SprintPos = Vector(0, 1, 0.5)
+SWEP.SprintPos = Vector(-1, 3, 1)
 SWEP.SprintAng = Angle(0, 0, 0)
-
-SWEP.InBipodPos = Vector(-4, 5, -1.5)
-SWEP.InBipodAng = Angle(0.3, 0, 0)
 
 /*
 SWEP.SprintPos = Vector(4, 2, 2)
@@ -134,8 +129,8 @@ SWEP.SprintAng = Angle(-15, 30.016, 0)
 SWEP.HolsterPos = Vector(0.532, -6, 0)
 SWEP.HolsterAng = Angle(-7.036, 30.016, 0)
 
-SWEP.CustomizePos = Vector(13, 0, 0)
-SWEP.CustomizeAng = Angle(15, 40, 30)
+SWEP.CustomizePos = Vector(13, 0, -2)
+SWEP.CustomizeAng = Angle(15, 40, 20)
 
 SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
 SWEP.BarrelOffsetHip = Vector(2, 0, -2)
@@ -145,27 +140,31 @@ SWEP.BarrelLength = 50
 SWEP.AttachmentElements = {
     ["bo1_bipod"] = {
         VMBodygroups = {
-            {ind = 3, bg = 1}
+            {ind = 2, bg = 1}
         }
     },
-    ["bo2_fastmag"] = {
+    ["extendedmag"] = {
         VMBodygroups = {
             {ind = 1, bg = 1}
         },
     },
     ["ammo_papunch"] = {
         NamePriority = 10,
-        NameChange = "Dead Specimen Reactor 5000",
+        NameChange = "L115 Isolator",
     },
-    ["muzzledev"] = {
-        VMBodygroups = {
-            {ind = 4, bg = 2}
+    ["mount"] = {
+        VMElements = {
+            {
+                Model = "models/weapons/arccw/item/bo1_ak_rail.mdl",
+                Bone = "tag_weapon",
+                Scale = Vector(0.35, 0.35, 0.375),
+                Offset = {
+                    pos = Vector(5.75, 0.275, 2.5),
+                    ang = Angle(0, 90, 0),
+                }
+            }
         },
-    },
-    ["rail"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 1}
-        },
+        ExcludeFlags = {"susat_norail"},
     },
 }
 
@@ -175,29 +174,15 @@ SWEP.Attachments = {
     { --1
         PrintName = "Optic", -- print name
         DefaultAttName = "Iron Sights",
-        Slot = {"bo2_dsr50_scope"}, -- what kind of attachments can fit here, can be string or table
+        Slot = {"bo1_susat"}, -- what kind of attachments can fit here, can be string or table
         Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
         Offset = {
-            vpos = Vector(-5, 0, 4.5),
+            vpos = Vector(5, 0, 3.5),
             vang = Angle(0, 0, 0),
         },
-        MergeSlots = {2},
+        MergeSlots = {10},
         CorrectivePos = Vector(0, 0, 0),
         CorrectiveAng = Angle(0.5, 0.05, 0),
-        InstalledEles = {"rail"},
-        Installed = "optic_bo2_dsr50"
-    },
-    { --2
-        Hidden = true,
-        Slot = {"optic_sniper", "optic"},
-        Bone = "tag_weapon",
-        Offset = {
-            vpos = Vector(0, 0, 3.225),
-            vang = Angle(0, 0, 0),
-        },
-        CorrectivePos = Vector(0, 0, 0),
-        CorrectiveAng = Angle(0.5, 0, 0),
-        InstalledEles = {"rail"},
     },
     { --3
         PrintName = "Muzzle",
@@ -207,38 +192,50 @@ SWEP.Attachments = {
         Slot = "muzzle",
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(24.2, 0, 2.1),
+            vpos = Vector(38.5, 0, 2.7),
             vang = Angle(0, 0, 0),
+            wpos = Vector(0, 0, 0),
+            wang = Angle(0, 0, 0)
         },
-        InstalledEles = {"muzzledev"}
     },
     { --4
         PrintName = "Underbarrel",
         Slot = {"foregrip"},
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(13, 0, -0.3),
+            vpos = Vector(16, 0, 0.75),
             vang = Angle(0, 0, 0),
+            wpos = Vector(20, 0.602, -6.5),
+            wang = Angle(-15, 0, 180)
         },
-        MergeSlots = {5},
+        MergeSlots = {5,6},
     },
     { --5
         Hidden = true,
-        Slot = {"bo1_bipod"},
+        Slot = {"bipod"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(18, 0, 1),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(25, 0.602, -7.75),
+            wang = Angle(-15, 0, 180)
+        },
     },
     { --6
+        Hidden = true,
+        Slot = {"bo1_bipod"},
+        Installed = "bo1_bipod_integral"
+    },
+    { --7
         PrintName = "Tactical",
         Slot = {"bo1_tacprimary"},
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(10, 0, 3.225),
-            vang = Angle(0, 0, 180),
+            vpos = Vector(16, -0.8, 1.5),
+            vang = Angle(0, 0, 90),
+            wpos = Vector(10.25, 1, -4.75),
+            wang = Angle(-12, 0, -180)
         },
-    },
-    { --7
-        PrintName = "Magazine",
-        Slot = {"bo2_fastmag"},
-        DefaultAttName = "Standard Magazine",
     },
     { --8
         PrintName = "Ammo Type",
@@ -248,37 +245,49 @@ SWEP.Attachments = {
         PrintName = "Perk",
         Slot = {"bo1_perk"},
     },
-    { --10
+    {
         PrintName = "Charm",
         Slot = "charm",
         FreeSlot = true,
         Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
         Offset = {
-            vpos = Vector(4, -0.8, 2),
+            vpos = Vector(0, -0.8, 1.5),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(10.25, 1, -4.75),
+            wang = Angle(-12, 0, -180)
+        },
+    },
+    {
+        Hidden = true,
+        Slot = {"optic_sniper", "optic"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(5, 0, 3.65),
             vang = Angle(0, 0, 0),
         },
+        InstalledEles = {"mount"},
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(0.5, 0.05, 0),
     },
 }
 
 SWEP.Hook_ModifyBodygroups = function(wep, data)
     local vm = data.vm
-    local papcamo = wep:GetBuff_Override("PackAPunch")
+    local papcamo = wep.Attachments[8].Installed == "ammo_papunch"
 
     if papcamo then
         return vm:SetSkin(2)
     end
 end
 
-SWEP.Hook_SelectReloadAnimation = function(wep, curanim)
-    local fastmag = wep:GetBuff_Override("BO1_FastMag")
-
-    if fastmag then
-        if curanim == "reload" then
-            return "fast"
-        elseif curanim == "reload_empty" then
-            return "fast_empty"
+SWEP.Hook_SelectReloadAnimation = function(wep, anim)
+    if wep:GetCapacity() == wep.ExtendedClipSize then
+        if anim == "reload" then
+            return "reload_ext"
+        elseif anim == "reload_empty" then
+            return "reload_empty_ext"
         end
-    end
+    else return end
 end
 
 SWEP.Animations = {
@@ -288,7 +297,7 @@ SWEP.Animations = {
     },
     ["draw"] = {
         Source = "draw",
-        Time = 1,
+        Time = 1.25,
         LHIK = true,
         LHIKIn = 0.2,
         LHIKOut = 0.25,
@@ -301,8 +310,14 @@ SWEP.Animations = {
         LHIKOut = 0.25,
     },
     ["ready"] = {
-        Source = "draw",
-        Time = 1,
+        Source = "first_draw",
+        Time = 2,
+        SoundTable = {
+            {s = "ArcCW_BO1.L96_BoltUp", t = 0.3},
+            {s = "ArcCW_BO1.L96_BoltBack", t = 0.5},
+            {s = "ArcCW_BO1.L96_BoltFwd", t = 1},
+            {s = "ArcCW_BO1.L96_BoltDown", t = 1.2},
+        },
         LHIK = true,
         LHIKIn = 0,
         LHIKOut = 0.25,
@@ -319,22 +334,28 @@ SWEP.Animations = {
         Source = "cycle",
         Time = 35 / 30, -- 45 / 30 ; 30 / 30
         ShellEjectAt = 0.5,
+        LHIK = true,
+        LHIKIn = 0.15,
+        LHIKOut = 0.15,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_BoltUp", t = 6 / 30}, -- 9 / 30 ; 6 / 30
-            {s = "ArcCW_BO2.Sniper_BoltBack", t = 10 / 30}, -- 15 / 30 ; 10 / 30
-            {s = "ArcCW_BO2.Sniper_BoltFwd", t = 20 / 30}, -- 30 / 30 ; 20 / 30
-            {s = "ArcCW_BO2.Sniper_BoltDown", t = 24 / 30}, -- 36 / 30 ; 24 / 30
+            {s = "ArcCW_BO1.L96_BoltUp", t = 6 / 30}, -- 9 / 30 ; 6 / 30
+            {s = "ArcCW_BO1.L96_BoltBack", t = 10 / 30}, -- 15 / 30 ; 10 / 30
+            {s = "ArcCW_BO1.L96_BoltFwd", t = 20 / 30}, -- 30 / 30 ; 20 / 30
+            {s = "ArcCW_BO1.L96_BoltDown", t = 24 / 30}, -- 36 / 30 ; 24 / 30
         },
     },
     ["cycle_iron"] = {
         Source = "cycle_ads",
         Time = 35 / 30, -- 45 / 30 ; 30 / 30
         ShellEjectAt = 0.5,
+        LHIK = true,
+        LHIKIn = 0.15,
+        LHIKOut = 0.15,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_BoltUp", t = 6 / 30}, -- 9 / 30 ; 6 / 30
-            {s = "ArcCW_BO2.Sniper_BoltBack", t = 10 / 30}, -- 15 / 30 ; 10 / 30
-            {s = "ArcCW_BO2.Sniper_BoltFwd", t = 20 / 30}, -- 30 / 30 ; 20 / 30
-            {s = "ArcCW_BO2.Sniper_BoltDown", t = 24 / 30}, -- 36 / 30 ; 24 / 30
+            {s = "ArcCW_BO1.L96_BoltUp", t = 6 / 30}, -- 9 / 30 ; 6 / 30
+            {s = "ArcCW_BO1.L96_BoltBack", t = 10 / 30}, -- 15 / 30 ; 10 / 30
+            {s = "ArcCW_BO1.L96_BoltFwd", t = 20 / 30}, -- 30 / 30 ; 20 / 30
+            {s = "ArcCW_BO1.L96_BoltDown", t = 24 / 30}, -- 36 / 30 ; 24 / 30
         },
     },
     ["reload"] = {
@@ -344,13 +365,12 @@ SWEP.Animations = {
         Checkpoints = {33, 55},
         FrameRate = 30,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_ClipOut", t = 27 / 30},
-            {s = "ArcCW_BO2.Sniper_ClipIn", t = 80 / 30},
+            {s = "ArcCW_BO1.L96_ClipOut", t = 27 / 30},
+            {s = "ArcCW_BO1.L96_ClipIn", t = 88 / 30},
         },
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.5,
-        MinProgress = 90 / 30
     },
     ["reload_empty"] = {
         Source = "reload_empty",
@@ -359,49 +379,48 @@ SWEP.Animations = {
         Checkpoints = {33, 55, 88},
         FrameRate = 30,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_BoltUp", t = 9 / 30},
-            {s = "ArcCW_BO2.Sniper_BoltBack", t = 13 / 30},
-            {s = "ArcCW_BO2.Sniper_ClipOut", t = 60 / 30},
-            {s = "ArcCW_BO2.Sniper_ClipIn", t = 113 / 30},
-            {s = "ArcCW_BO2.Sniper_BoltFwd", t = 155 / 30},
-            {s = "ArcCW_BO2.Sniper_BoltDown", t = 159 / 30},
+            {s = "ArcCW_BO1.L96_BoltUp", t = 9 / 30},
+            {s = "ArcCW_BO1.L96_BoltBack", t = 13 / 30},
+            {s = "ArcCW_BO1.L96_ClipOut", t = 60 / 30},
+            {s = "ArcCW_BO1.L96_ClipIn", t = 121 / 30},
+            {s = "ArcCW_BO1.L96_BoltFwd", t = 155 / 30},
+            {s = "ArcCW_BO1.L96_BoltDown", t = 159 / 30},
         },
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.5,
-        MinProgress = 160 / 30
     },
-    ["fast"] = {
-        Source = "fast",
-        Time = 120 / 35,
+    ["reload_ext"] = {
+        Source = "reload_ext",
+        Time = 120 / 30,
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        Checkpoints = {33, 55},
         FrameRate = 30,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_ClipOut", t = 27 / 35},
-            {s = "ArcCW_BO2.Sniper_ClipIn", t = 80 / 35},
+            {s = "ArcCW_BO1.L96_ClipOut", t = 27 / 30},
+            {s = "ArcCW_BO1.L96_ClipIn", t = 88 / 30},
         },
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.5,
-        MinProgress = 90 / 35
     },
-    ["fast_empty"] = {
-        Source = "fast_empty",
-        Time = 173 / 35,
+    ["reload_empty_ext"] = {
+        Source = "reload_empty_ext",
+        Time = 173 / 30,
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        Checkpoints = {33, 55, 88},
         FrameRate = 30,
         SoundTable = {
-            {s = "ArcCW_BO2.Sniper_BoltUp", t = 9 / 35},
-            {s = "ArcCW_BO2.Sniper_BoltBack", t = 13 / 35},
-            {s = "ArcCW_BO2.Sniper_ClipOut", t = 60 / 35},
-            {s = "ArcCW_BO2.Sniper_ClipIn", t = 121 / 35},
-            {s = "ArcCW_BO2.Sniper_BoltFwd", t = 155 / 35},
-            {s = "ArcCW_BO2.Sniper_BoltDown", t = 159 / 35},
+            {s = "ArcCW_BO1.L96_BoltUp", t = 9 / 30},
+            {s = "ArcCW_BO1.L96_BoltBack", t = 13 / 30},
+            {s = "ArcCW_BO1.L96_ClipOut", t = 60 / 30},
+            {s = "ArcCW_BO1.L96_ClipIn", t = 121 / 30},
+            {s = "ArcCW_BO1.L96_BoltFwd", t = 155 / 30},
+            {s = "ArcCW_BO1.L96_BoltDown", t = 159 / 30},
         },
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.5,
-        MinProgress = 160 / 35
     },
     ["enter_sprint"] = {
         Source = "sprint_in",
@@ -409,7 +428,7 @@ SWEP.Animations = {
     },
     ["idle_sprint"] = {
         Source = "sprint_loop",
-        Time = 30 / 35
+        Time = 30 / 40
     },
     ["exit_sprint"] = {
         Source = "sprint_out",
