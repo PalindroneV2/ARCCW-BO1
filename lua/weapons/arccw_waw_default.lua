9105d71b60f16693fe986988f8b46044dcd7c004
PalindroneV2 <asfadasfag@gmail.com>
2021-10-18 23:44:56 -0700

    BRAINS and Default Gun

---
+++ lua/weapons/arccw_waw_default.lua
@@ -0,0 +1,183 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = false -- this obviously has to be set to true
+SWEP.Category = "ArcCW - World at War" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "Finger Gun"
+SWEP.Trivia_Class = "Pistol"
+SWEP.Trivia_Desc = "You ever wanted to actually shoot people with your finger? Go for it."
+SWEP.Trivia_Manufacturer = "N/A"
+SWEP.Trivia_Calibre = "Imaginary"
+SWEP.Trivia_Mechanism = "Imagination"
+SWEP.Trivia_Country = "Any"
+SWEP.Trivia_Year = "Any"
+
+SWEP.Slot = 1
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/c_waw_default.mdl"
+SWEP.WorldModel = "models/weapons/arccw/c_bo1_m1911.mdl"
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    scale = 1.025,
+    pos        =    Vector(-10, 3, -3.5),
+    ang        =    Angle(-6, -2.5, 180),
+    bone    =    "ValveBiped.Bip01_R_Hand",
+}
+SWEP.ViewModelFOV = 60
+
+SWEP.Damage = 50
+SWEP.DamageMin = 15
+SWEP.RangeMin = 10
+SWEP.Range = 100
+
+SWEP.Penetration = 0
+SWEP.DamageType = DMG_BULLET
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 250 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.CanFireUnderwater = true
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerCol = Color(0,0,0)
+SWEP.TracerWidth = 3
+
+SWEP.ChamberSize = 0 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 8 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 24
+
+SWEP.Recoil = 0
+SWEP.RecoilSide = 0
+SWEP.RecoilRise = 0
+
+SWEP.Delay = 60 / 120 -- 60 / RPM.
+SWEP.Num = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = "weapon_pistol"
+SWEP.NPCWeight = 100
+
+SWEP.AccuracyMOA = 0 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 0 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 0
+
+SWEP.ShootWhileSprint = false
+
+SWEP.Primary.Ammo = "pistol" -- what ammo type the gun uses
+SWEP.MagID = "bhp" -- the magazine pool this gun draws from
+
+SWEP.ShootVol = 115 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+
+SWEP.ShootSound = ""
+SWEP.ShootSoundSilenced = ""
+SWEP.DistantShootSound = ""
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
+SWEP.ShellScale = 1.5
+SWEP.ShellPitch = 90
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = nil -- which attachment to put the case effect on
+SWEP.ProceduralViewBobAttachment = 1
+SWEP.CamAttachment = 1
+
+SWEP.SightTime = 0.175
+
+SWEP.SpeedMult = 1
+SWEP.SightedSpeedMult = 0.75
+
+SWEP.BarrelLength = 16
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+    -- [0] = "bulletchamber",
+    -- [1] = "bullet1"
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = false
+
+SWEP.HoldtypeHolstered = "normal"
+SWEP.HoldtypeActive = "pistol"
+SWEP.HoldtypeSights = "revolver"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.SprintPos = Vector(0, 0, 0)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.CustomizePos = Vector(0, 0, 0)
+SWEP.CustomizeAng = Angle(0, 0, 0)
+
+SWEP.HolsterPos = Vector(0, 0, 0)
+SWEP.HolsterAng = Angle(0, 0, 0)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.AttachmentElements = {
+}
+
+SWEP.ExtraSightDist = 2
+
+SWEP.RejectAttachments = {
+}
+
+SWEP.Attachments = {
+}
+
+SWEP.Hook_ModifyBodygroups = function(wep, data)
+end
+
+SWEP.Hook_TranslateAnimation = function(wep, anim, data)
+end
+
+SWEP.Hook_GetShootSound = function(wep, sound)
+end
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1 / 30,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 0.75,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["ready"] = {
+        Source = "draw",
+        Time = 0.75,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["fire"] = {
+        Source = {"fire"},
+        Time = 7 / 30,
+        ShellEjectAt = 0,
+    },
+    ["reload"] = {
+        Source = "idle",
+        Time = 1 / 30,
+    },
+}
---
+++ lua/weapons/arccw_waw_zombie.lua
@@ -0,0 +1,108 @@
+SWEP.Base = "arccw_base_melee"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - World at War" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "BRAAAINS..."
+SWEP.Trivia_Desc = [[
+    You've become infected with 115! Now you're nothing but a brainless puppet in search of flesh to devour.
+]]
+SWEP.Trivia_Class = "Melee Weapon"
+SWEP.Trivia_Manufacturer = "N/A"
+SWEP.Trivia_Calibre = "N/A"
+SWEP.Trivia_Mechanism = "Clawing"
+SWEP.Trivia_Country = "N/A"
+SWEP.Trivia_Year = "Any"
+
+SWEP.Slot = 0
+
+SWEP.NotForNPCs = false
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/c_waw_zombie.mdl"
+SWEP.WorldModel = ""
+SWEP.ViewModelFOV = 60
+
+SWEP.ProceduralViewBobAttachment = 1
+SWEP.CamAttachment = 1
+
+SWEP.PrimaryBash = true
+SWEP.CanBash = true
+
+SWEP.MeleeDamage = 50
+SWEP.MeleeRange = 25
+SWEP.MeleeTime = 0.75
+SWEP.MeleeAttackTime = 0.5
+SWEP.MeleeDamageType = DMG_CLUB
+
+SWEP.MeleeSwingSound = "weapons/arccw/knife/knife_slash2.wav"
+SWEP.MeleeHitSound = "physics/body/body_medium_impact_hard1.wav"
+SWEP.MeleeHitNPCSound = "physics/body/body_medium_impact_hard5.wav"
+
+SWEP.NotForNPCs = false
+
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+        PrintName = "BRAAAAAAINS"
+    },
+}
+
+SWEP.MeleeGesture = ACT_HL2MP_GESTURE_RANGE_ATTACK_FIST
+
+SWEP.HoldtypeActive = "fist"
+SWEP.HoldtypeHolstered = "normal"
+
+SWEP.Primary.ClipSize = -1
+
+-- 0	=	reference
+-- 1	=	seq_admire
+-- 2	=	fists_draw
+-- 3	=	fists_right
+-- 4	=	fists_left
+-- 5	=	fists_uppercut
+-- 6	=	fists_holster
+-- 7	=	fists_idle_01
+-- 8	=	fists_idle_02
+
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = {"idle"},
+        Time = 1 / 30,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 0.5,
+    },
+    ["ready"] = {
+        Source = "draw",
+        Time = 1,
+    },
+    ["holster"] = {
+        Source = "holster",
+        Time = 0.5,
+    },
+    ["bash"] = {
+        Source = {"attack"},
+        Time = 0.75,
+    },
+}
+
+SWEP.IronSightStruct = false
+
+SWEP.ActivePos = Vector(0, 3, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.CustomizePos = Vector(0, 3, 0)
+SWEP.CustomizeAng = Angle(0, 0, 0)
+
+SWEP.BashPreparePos = Vector(0, 0, 0)
+SWEP.BashPrepareAng = Angle(0, 0, 0)
+
+SWEP.BashPos = Vector(0, 0, 0)
+SWEP.BashAng = Angle(0, 0, 0)
+
+SWEP.HolsterPos = Vector(0, 0, 0)
+SWEP.HolsterAng = Angle(0, 0, 0)
