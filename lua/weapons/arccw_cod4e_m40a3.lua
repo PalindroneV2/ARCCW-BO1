10308be460d032731c38230cb1e82be053609a97
PalindroneV2 <asfadasfag@gmail.com>
2021-11-04 16:07:09 -0700

    M40 no sounds yet

---
+++ lua/weapons/arccw_cod4e_m40a3.lua
@@ -0,0 +1,409 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - COD Extras" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "M40A3"
+SWEP.Trivia_Class = "Rifle"
+SWEP.Trivia_Desc = [[
+    American bolt action rifle built from a Remington 700 with the wooden stock replaced by a fiberglass stock produced by McMillan.
+]]
+SWEP.Trivia_Manufacturer = "Remington"
+SWEP.Trivia_Calibre = "7.62x51mm NATO"
+SWEP.Trivia_Mechanism = "Bolt-Action"
+SWEP.Trivia_Country = "USA"
+SWEP.Trivia_Year = 1966
+
+SWEP.Slot = 3
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/c_cod4_m40a3.mdl"
+SWEP.WorldModel = "models/weapons/arccw/c_cod4_m40a3.mdl"
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    pos        =    Vector(-5.5, 3.5, -5),
+    ang        =    Angle(-10, 1, 180),
+    bone    =    "ValveBiped.Bip01_R_Hand",
+    scale   =   0.9
+}
+SWEP.ViewModelFOV = 60
+
+SWEP.DefaultBodygroups = "0000000000"
+
+SWEP.Damage = 100
+SWEP.DamageMin = 75 -- damage done at maximum range
+SWEP.Range = 100 -- in METRES
+SWEP.Penetration = 7
+SWEP.DamageType = DMG_BULLET
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 850 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerCol = Color(255, 25, 25)
+SWEP.TracerWidth = 3
+
+SWEP.ChamberSize = 0 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 5 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 15
+
+SWEP.Recoil = 1
+SWEP.RecoilSide = 0.75
+SWEP.RecoilRise = 0.75
+SWEP.VisualRecoilMult = 0
+
+SWEP.Delay = 60 / 45-- 60 / RPM.
+SWEP.Num = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = {
+    "weapon_ar2",
+    "weapon_crossbow",
+}
+SWEP.NPCWeight = 100
+
+SWEP.AccuracyMOA = 0.5 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 800 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 250
+
+SWEP.Primary.Ammo = "ar2" -- what ammo type the gun uses
+SWEP.MagID = "m40" -- the magazine pool this gun draws from
+
+SWEP.ShootVol = 115 -- volume of shoot sound
+
+SWEP.ShootSound = "ArcCW_WAW.Springfield_Fire"
+SWEP.ShootSoundSilenced = "ArcCW_BO1.FAL_Sil"
+SWEP.DistantShootSound = "ArcCW_WAW.K98_Ringoff"
+
+SWEP.MuzzleEffect = "muzzleflash_4"
+SWEP.ShellModel = "models/shells/shell_556.mdl"
+SWEP.ShellPitch = 90
+SWEP.ShellScale = 1.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+SWEP.ProceduralViewBobAttachment = 1
+SWEP.CamAttachment = 3
+
+SWEP.SpeedMult = 0.85
+SWEP.SightedSpeedMult = 0.5
+SWEP.SightTime = 0.4
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+SWEP.ShotgunReload = true
+SWEP.ManualAction = true
+
+SWEP.CaseBones = {
+}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-2.15, -3, 2.1),
+    Ang = Angle(0.1, -0.05, 0),
+    Magnification = 1.25,
+    CrosshairInSights = false,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "ar2"
+SWEP.HoldtypeSights = "rpg"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.SprintPos = Vector(0, 0, 0)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.CustomizePos = Vector(15, 3, 1)
+SWEP.CustomizeAng = Angle(15, 40, 30)
+
+SWEP.HolsterPos = Vector(3, 0, 0)
+SWEP.HolsterAng = Angle(-7.036, 30.016, 0)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.BarrelLength = 30
+
+SWEP.ExtraSightDist = 5
+
+SWEP.AttachmentElements = {
+    ["bo1_bipod"] = {
+        VMBodygroups = {
+            {ind = 1, bg = 1},
+        },
+    },
+    ["ubthing"] = {
+        ExcludeFlags = {"bo1_bipod"},
+        VMElements = {
+            {
+                Model = "models/weapons/arccw/item/bo2_rail.mdl",
+                Bone = "tag_weapon",
+                Scale = Vector(0.4, 0.4, 0.4),
+                Offset = {
+                    pos = Vector(4, 0, -0.1),
+                    ang = Angle(0, 0, 180),
+                }
+            },
+        },
+    },
+    ["papname1"] = {
+        SightTime = 25 / 40
+    },
+}
+
+SWEP.Attachments = {
+    { --1
+        PrintName = "Optic", -- print name
+        DefaultAttName = "Iron Sights",
+        Slot = {"optic"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "tag_weapon", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(-3.5, 0, 2.4), -- 4.6 offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+        },
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(1.5, 0, 0),
+        InstalledEles = {"mount"},
+        MergeSlots = {8}
+    },
+    { --2
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Slot = {"muzzle"},
+        VMScale = Vector(1.75, 1.1, 1.1),
+        WMScale = Vector(2, 1.1, 1.1),
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(26, 0, 1.175), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+            wpos = Vector(0, 0, 0),
+            wang = Angle(0, 0, 0)
+        },
+    },
+    {
+        PrintName = "Underbarrel",
+        Slot = {"foregrip", "bo1_bipod"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(3.5, 0, -0.2), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+        },
+        InstalledEles = {"ubthing"},
+    }, --3
+    {
+        PrintName = "Tactical",
+        Slot = {"bo1_tacprimary"},
+        VMScale = Vector(0.75, 0.75, 0.75),
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(6, 0.6, 0.75), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, -90),
+        },
+    }, --4
+    {
+        PrintName = "Ammo Type",
+        Slot = {"ammo_pap"}
+    }, --5
+    {
+        PrintName = "Perk",
+        Slot = {"bo1_perk"}
+    }, --6
+    {
+        PrintName = "Charm",
+        Slot = "charm",
+        FreeSlot = true,
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(-0.5, -0.85, 0.25),
+            vang = Angle(0, 0, 0),
+        },
+    }, --7
+    {
+        Hidden = true,
+        Slot = "cod4_m40_scope",
+        Bone = "j_gun", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(0, 0, 0), -- 4.6 offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+        },
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(0, 0, 0),
+        InstalledEles = {"optic_cod4_m40scope"},
+    },--8
+}
+
+SWEP.Hook_NameChange = function(wep, name)
+    local pap = wep:GetBuff_Override("PackAPunch")
+
+    if pap then
+        return "Quickscoper"
+    end
+end
+
+SWEP.Hook_SelectInsertAnimation = function(wep, data)
+    local pap = wep:GetBuff_Override("PackAPunch")
+
+    if pap then
+        return {count = 8, anim = "sgreload_insert_pap"}
+    end
+end
+
+
+SWEP.Hook_ModifyBodygroups = function(wep, data)
+    local vm = data.vm
+    local papcamo = wep:GetBuff_Override("PackAPunch")
+
+    if papcamo then return vm:SetSkin(2) end
+end
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1 / 30,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 0.75,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["holster"] = {
+        Source = "holster",
+        Time = 0.5,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["ready"] = {
+        Source = "draw",
+        Time = 0.75,
+        LHIK = true,
+        LHIKIn = 0.25,
+        LHIKOut = 0.25,
+    },
+    ["fire"] = {
+        Source = {"fire"},
+        Time = 7 / 30,
+        {s = "ArcCW_WAW.Springfield_Mech", t = 0 / 30},
+    },
+    ["cycle"] = {
+        Source = {"cycle"},
+        Time = 28 / 30,
+        ShellEjectAt = 10 / 30,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Up", t = 5 / 30},
+            {s = "ArcCW_WAW.Arisaka_Back", t = 10 / 30},
+            {s = "ArcCW_WAW.Arisaka_Fwd", t = 20 / 30},
+            {s = "ArcCW_WAW.Arisaka_Down", t = 22 / 30},
+        },
+    },
+    ["fire_iron"] = {
+        Source = {"fire_ads"},
+        Time = 7 / 30,
+        {s = "ArcCW_WAW.Springfield_Mech", t = 1 / 30},
+    },
+    ["cycle_ads"] = {
+        Source = {"cycle"},
+        Time = 28 / 30,
+        ShellEjectAt = 10 / 30,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Up", t = 5 / 30},
+            {s = "ArcCW_WAW.Arisaka_Back", t = 10 / 30},
+            {s = "ArcCW_WAW.Arisaka_Fwd", t = 20 / 30},
+            {s = "ArcCW_WAW.Arisaka_Down", t = 22 / 30},
+        },
+    },
+
+    --SGRELOAD--
+    ["sgreload_start"] = {
+        Source = "reload_in",
+        Time = 60 / 30,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        RestoreAmmo = 1, -- loads a shell since the first reload has a shell in animation
+        MinProgress = 1,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 4,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Up", t = 5 / 30},
+            {s = "ArcCW_WAW.Arisaka_Back", t = 10 / 30},
+            {s = "ArcCW_WAW.Arisaka_Insert", t = 35 / 30},
+        },
+    },
+    ["sgreload_insert"] = {
+        Source = "reload_loop",
+        Time = 26 / 40,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        TPAnimStartTime = 0.3,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 4,
+        MinProgress = 3 / 30,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Insert", t = 3 / 30},
+        }
+    },
+    ["sgreload_insert_pap"] = {
+        Source = "reload_loop",
+        Time = 26 / 40,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        TPAnimStartTime = 0.3,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 4,
+        MinProgress = 3 / 30,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Insert", t = 3 / 30},
+        }
+    },
+    ["sgreload_finish"] = {
+        Source = "reload_out",
+        Time = 26 / 30,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 4,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Fwd", t = 5 / 30},
+            {s = "ArcCW_WAW.Arisaka_Down", t = 10 / 30},
+        },
+    },
+    ["sgreload_finish_empty"] = {
+        Source = "reload_out",
+        Time = 26 / 30,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 4,
+        SoundTable = {
+            {s = "ArcCW_WAW.Arisaka_Fwd", t = 5 / 30},
+            {s = "ArcCW_WAW.Arisaka_Down", t = 10 / 30},
+        },
+    },
+    ["enter_sprint"] = {
+        Source = "sprint_in",
+        Time = 10 / 30
+    },
+    ["idle_sprint"] = {
+        Source = "sprint_loop",
+        Time = 30 / 40
+    },
+    ["exit_sprint"] = {
+        Source = "sprint_out",
+        Time = 10 / 30
+    },
+}
