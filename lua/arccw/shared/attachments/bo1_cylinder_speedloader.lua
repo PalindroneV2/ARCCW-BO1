42bfc3529756e8e808c7677581b09a06fd7f0927
TheOnly8Z <888888zombies@gmail.com>
2021-11-21 16:09:32 -0500

    speedloader dumps clip

--- lua/arccw/shared/attachments/bo1_cylinder_speedloader.lua
+++ lua/arccw/shared/attachments/bo1_cylinder_speedloader.lua
@@ -2,7 +2,10 @@ att.PrintName = "Speedloader"
 att.Icon = Material("entities/acwatt_bo1_ammo_phyton_speed.png", "mips smooth")
 att.Description = "A clip that can insert all rounds of a cylinder at once, speeding up reload."
 att.Desc_Pros = {
-    "bo1.speedloader"
+    "bo1.speedloader.pro"
+}
+att.Desc_Cons = {
+    "bo1.speedloader.con"
 }
 att.AutoStats = true
 att.Slot = {"bo1_cylinder"}
@@ -11,5 +14,6 @@ att.GivesFlags = {"bo1_speedloader"}
 att.BO1_SpeedLoader = true
 att.Override_ShotgunReload = false
 
-att.Mult_DrawTime = 1.25
-att.Mult_HolsterTime = 1.25
+att.Hook_ReloadDumpClip = function(wep)
+    return true
+end
